85c6523f5752dc5b6fdbcde03e06d42a3df0938b
unknown <smartkelvin66@gmail.com>
2024-05-05 06:01:12 +0100

    updated-mint

---
+++ NFT/script/Interaction.s.sol
@@ -0,0 +1,26 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+// import {Script} from  "forge-std/Script.sol";
+// import {DevOpsTools} from "../lib/foundry-devops/src/DevOpsTools";
+import {DevOpsTools} from "../lib/foundry-devops/src/DevOpsTools.sol";
+
+import {BasicNft} from "../src/BasicNft.sol";
+import {Script} from "../lib/forge-std/src/Script.sol";
+
+contract MintBasicNft is Script {
+    string public constant PUG_URI =
+        "ipfs://bafybeig37ioir76s7mg5oobetncojcm3c3hxasyd4rvid4jqhy4gkaheg4/?filename=0-PUG.json";
+    uint256 deployerKey;
+
+    function run() external {
+        address mostRecentlyDeployedBasicNft = DevOpsTools
+            .get_most_recent_deployment("BasicNft", block.chainid);
+        mintNftOnContract(mostRecentlyDeployedBasicNft);
+    }
+
+    function mintNftOnContract(address basicNftAddress) public {
+        vm.startBroadcast();
+        BasicNft(basicNftAddress).mintNft(PUG_URI);
+        vm.stopBroadcast();
+    }
+}
